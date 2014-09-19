b4d7c2a43ff1031301e45b1fdd70b818ac565715
inkarri <miguel.amaya99@gmail.com>
2014-09-19 03:36:52 -0500

    arreglando errores

--- tienda/settings.py
+++ tienda/settings.py
@@ -171,7 +171,7 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_URL = 'facturacion/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '../facturacion/static'),
