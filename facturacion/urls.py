03d0631a79f1b78b9f8fa5d17815048a254e9560
inkarri <miguel.amaya99@gmail.com>
2014-09-19 11:48:52 -0500

    arreglando errores

--- facturacion/urls.py
+++ facturacion/urls.py
@@ -18,4 +18,6 @@ urlpatterns = patterns('',
 	#url(r'^(?P<encuesta_id>\d+)/voto/$',views.voto, name='voto'),
 )
 if not settings.DEBUG:
-urlpatterns += patterns('',(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),)
+	urlpatterns += patterns('',
+		(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+		)
